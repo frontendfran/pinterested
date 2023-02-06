646c0ce1502e6f58844313c8cb69284e4deead2d
frontendfran <hello@franhaines.com>
2023-02-06 21:57:56 +0000

    Have added Join and Login functionality with Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Added as per Devise installation instructions
+  config.action_mailer.default_url_options = { host: 'terested-ruby-on-rails.herokuapp.com' }
 end
